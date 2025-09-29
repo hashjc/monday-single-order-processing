539d0e6745e9e36c079a93605eec080b5a7b685a
ankit5496 <44693148+ankit5496@users.noreply.github.com>
2025-09-29 14:55:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -124,4 +124,6 @@ def generate_labels():
 
 
 if __name__  == '__main__':
-    app.run(debug=True, port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True, port=8000)
